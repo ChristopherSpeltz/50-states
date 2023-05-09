76cbf2d34adeb87e46dd6b6e106fa343b8b49cb1
Christopher Speltz <chrisspeltz@msn.com>
2023-05-08 23:51:21 -0500

    vue 50-states-statedetail components

--- client/src/components/StateList.vue
+++ client/src/components/StateList.vue
@@ -1,14 +1,17 @@
 <template>
-<div>
-    <p v-for="state in states" v-bind:key="state.name">
-    {{ state.name }}
-    </p>
+    <!-- code to bind state detail to objects in array -->
+<div class="state-list-container">
+    <div class="state-container" v-for="state in states" v-bind:key="state.name">
+    <state-detail v-bind:state="state"></state-detail>
+    </div>
 </div>
 </template>
 
 <script>
+import StateDetail from './StateDetail.vue'
 
 export default {
+    components: { StateDetail },
     name: 'StateList',
     data() {
         return {
@@ -29,5 +32,15 @@ export default {
 </script>
 
 <style scoped>
+
+.state-list-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+
+.state-container {
+    margin: 1rem;
+}
 
 </style>
