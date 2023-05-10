9f9a14579d35870f636f3a80a124c9e50687b4ea
Christopher Speltz <chrisspeltz@msn.com>
2023-05-09 23:23:22 -0500

    9-vue 50 states updating visited/not visited

--- client/src/components/StateList.vue
+++ client/src/components/StateList.vue
@@ -2,7 +2,10 @@
     <!-- code to bind state detail to objects in array -->
 <div class="state-list-container">
     <div class="state-container" v-for="state in states" v-bind:key="state.name">
-    <state-detail v-bind:state="state"></state-detail>
+    <state-detail
+    v-bind:state="state"
+    v-on:update-visited="updateVisited"
+    ></state-detail>
     </div>
 </div>
 </template>
@@ -25,6 +28,11 @@ export default {
         fetchAllStates() {
             this.$stateService.getAllStates().then( states => {
                 this.states = states
+            })
+        },
+        updateVisited(stateName, visited) {
+            this.$stateService.setVisited(stateName, visited).then( () => {
+                this.fetchAllStates()
             })
         }
     }
--- client/src/services/stateService.js
+++ client/src/services/stateService.js
@@ -6,5 +6,13 @@ export default {
         return axios.get('/api/states').then( response => {
             return response.data
         })
+    },
+
+    setVisited(stateName, visited) {
+        // example URL api/state/Wisconsin
+        let requestData = { visited: visited }
+        return axios.patch('/api/states/' + stateName, requestData).then( response => {
+            return response.data
+        })
     }
 }
