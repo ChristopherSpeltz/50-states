0cad0d0aea5df592dcae1af00a6232995278ac55
Christopher Speltz <chrisspeltz@msn.com>
2023-05-10 23:42:21 -0500

    state summary

--- client/src/components/StateList.vue
+++ client/src/components/StateList.vue
@@ -1,54 +1,74 @@
 <template>
-    <!-- code to bind state detail to objects in array -->
-<div class="state-list-container">
-    <div class="state-container" v-for="state in states" v-bind:key="state.name">
-    <state-detail
-    v-bind:state="state"
-    v-on:update-visited="updateVisited"
-    ></state-detail>
+  <div>
+    <div>
+      <state-summary v-bind:states="states"></state-summary>
     </div>
-</div>
+    <!-- code to bind state detail to objects in array -->
+    <div class="state-list-container">
+      <div
+        class="state-container"
+        v-for="state in states"
+        v-bind:key="state.name"
+      >
+        <state-detail
+          v-bind:state="state"
+          v-on:update-visited="updateVisited"
+        ></state-detail>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import StateDetail from './StateDetail.vue'
+import StateDetail from "./StateDetail.vue";
+import StateSummary from "./StateSummary";
 
 export default {
-    components: { StateDetail },
-    name: 'StateList',
-    data() {
-        return {
-            states: []
-        }
+  components: { StateDetail, StateSummary },
+  name: "StateList",
+  data() {
+    return {
+      states: [],
+    };
+  },
+  mounted() {
+    this.fetchAllStates();
+  },
+  methods: {
+    fetchAllStates() {
+      this.$stateService
+        .getAllStates()
+        .then((states) => {
+          this.states = states;
+        })
+        .catch((err) => {
+          alert("Sorry, can't fetch state list"); // error alert for user
+          console.error(err); // logging for developer
+        });
     },
-    mounted() {
-        this.fetchAllStates()
+    updateVisited(stateName, visited) {
+      this.$stateService
+        .setVisited(stateName, visited)
+        .then(() => {
+          this.fetchAllStates();
+        })
+        .catch((err) => {
+          alert("Sorry, can't update state"); // error alert for user
+          console.error(err); // logging for developer
+        });
     },
-    methods: {
-        fetchAllStates() {
-            this.$stateService.getAllStates().then( states => {
-                this.states = states
-            })
-        },
-        updateVisited(stateName, visited) {
-            this.$stateService.setVisited(stateName, visited).then( () => {
-                this.fetchAllStates()
-            })
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-
 .state-list-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 }
 
 .state-container {
-    margin: 1rem;
+  margin: 1rem;
 }
-
 </style>
