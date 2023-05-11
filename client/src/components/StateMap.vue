fe9a5ed209afc44b5d5725a9e0fa1ac6b71639e2
Christopher Speltz <chrisspeltz@msn.com>
2023-05-10 23:01:40 -0500

    update maps and handle 404 errors

--- client/src/components/StateMap.vue
+++ client/src/components/StateMap.vue
@@ -6,13 +6,12 @@
     <p v-if="state.visited">You have visited this state</p>
     <p v-else>You have not visited this state yet</p>
 
-    <!-- todo map here -->
-
-    <div id="map-container">
-        <l-map v-bind:center="mapCenter" v-bind:zoom="state.zoom">
+    <!-- code to display map -->
+    <div id="map-container" v-if="dataReady">
+        <l-map ref="map" v-on:ready="onMapReady" v-bind:center="mapCenter" v-bind:zoom="state.zoom">
             <l-tile-layer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy: OpenStreetMap contributors">
+            attribution="&copy; OpenStreetMap contributors">
             </l-tile-layer>
         </l-map>
     </div>
@@ -24,6 +23,7 @@
 
 import {LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
 
+
 export default {
     name: 'StateMap',
     components: {
@@ -31,7 +31,9 @@ export default {
     },
     data() {
         return {
-            state: {}
+            state: {},
+            dataReady: false,
+            mapReady: false
         }
     },
      mounted() {
@@ -41,8 +43,32 @@ export default {
      methods: {
         fetchStateData() {
             this.$stateService.getOneState(this.state.name).then( state => {
-                this.state =state
+                this.state = state
+                this.dataReady = true
+            }).catch( err => {
+                // 404 not found
+                if ( err.response && err.response.status === 404) {
+                    this.state.name = '?' //todo think about a better way to communicate to the user
+                } else {
+                    // 500 server error
+                    alert('Sorry, error fetching data about this state') // general message for user
+                    console.error(err) //for the developer
+                }
             })
+        },
+        onMapReady() {
+            this.mapReady = true
+        },
+        setMapView() {
+            if (this.mapReady && this.dataReady) {
+                // todo - make sure map shows correct part of world and zoom level.
+                this.$refs.map.leafletObject.setView(this.mapCenter, this.zoom)
+            }
+        }
+     },
+     computed: {
+        mapCenter() {
+            return [ this.state.lat, this.state.lon ]
         }
      }
 }
